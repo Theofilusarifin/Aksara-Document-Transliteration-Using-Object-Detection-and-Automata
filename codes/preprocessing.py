254965ca5c73aa9639e2f4a26663b2d661d86392
theofilusarifin <theofilusarifin@gmail.com>
2024-01-22 04:58:58 +0700

    Update

--- codes/preprocessing.py
+++ codes/preprocessing.py
@@ -6,8 +6,11 @@ class ImagePreprocessingError(Exception):
 
 def image_preprocessing_process(image):
     try:
+        cv2.imwrite('./images/preprocessing/1_original_image.jpg', cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+
         # convert to grayscale
         grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        cv2.imwrite('./images/preprocessing/2_grayscale_image.jpg', cv2.cvtColor(grayscale_image, cv2.COLOR_BGR2RGB))
 
     except Exception as e:
         # Handle error in converting to grayscale
@@ -16,6 +19,7 @@ def image_preprocessing_process(image):
     try:
         # otsu threshold
         thresholded_image = cv2.threshold(grayscale_image, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]
+        cv2.imwrite('./images/preprocessing/3_threshold_image.jpg', cv2.cvtColor(thresholded_image, cv2.COLOR_BGR2RGB))
 
     except Exception as e:
         # Handle error in Otsu thresholding
@@ -24,6 +28,7 @@ def image_preprocessing_process(image):
     try:
         # Bitwise not
         binary_image = cv2.bitwise_not(thresholded_image)
+        cv2.imwrite('./images/preprocessing/4_binary_image.jpg', cv2.cvtColor(binary_image, cv2.COLOR_BGR2RGB))
 
     except Exception as e:
         # Handle error in bitwise NOT operation
@@ -34,6 +39,7 @@ def image_preprocessing_process(image):
         kernel = np.ones((7, 7), np.uint8)
         # Perform dilation
         dilated_image = cv2.dilate(binary_image, kernel, iterations=1)
+        cv2.imwrite('./images/preprocessing/5_dilated_image.jpg', cv2.cvtColor(dilated_image, cv2.COLOR_BGR2RGB))
 
     except Exception as e:
         # Handle error in dilation
@@ -49,6 +55,8 @@ def image_preprocessing_process(image):
             area = cv2.contourArea(contour)
             if area < min_area_threshold:
                 cv2.drawContours(dilated_image, [contour], 0, 0, -1)
+        
+        cv2.imwrite('./images/preprocessing/6_contour_image.jpg', cv2.cvtColor(dilated_image, cv2.COLOR_BGR2RGB))
 
     except Exception as e:
         # Handle error in finding contours or contour filtering
